bee85d0fed1b83d0b9cccfefa1e41dfe2f7f5736
ymorales-galvan <ymorales-galvan@mail.csuchico.edu>
2019-10-07 17:43:20 -0700

    added more options that allowed user to add groceries to the list.

--- main.cpp
+++ main.cpp
@@ -5,19 +5,38 @@ using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input = ' ';
+        string list[5]; //array of 5 strings
+        string nameItem;
+        int numItems = 0;
+        char input = ' ';
 
-while (input != 'q' && input != 'Q' )
-{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  cin.ignore();
-}
- return 0;
+        do
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                cin.ignore();
+                if  (input == 'a' || input == 'A')
+
+                {
+                        cout<<"What is the item?\n";
+                        cin>>nameItem;
+                        cin.ignore();
+                if (numItems >=5)
+                {
+                        cout<<"You'll need a bigger list!\n";
+                }
+
+                numItems++;
+                }
+                
+//                string list[5] = { nameItem } ;
+        }
+
+        while (input != 'q' && input != 'Q' );
+
+        return 0;
 }
